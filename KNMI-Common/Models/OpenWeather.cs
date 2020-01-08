1ea68f5b50a8e54fc010561a828c59c9c6b81fe7
Kees Hiemstra <chi@xs4all.nl>
2020-01-08 19:04:13 +0100

    Preparing the WeatherMonitor

---
+++ KNMI-Common/Models/OpenWeather.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace WeatherMonitor.Models
+{
+	[DataContract]
+	public class OpenWeather
+	{
+		[DataMember]
+		public Coord coord { get; set; }
+
+		[DataMember]
+		public List<WeatherI> weather { get; set; }
+
+		[DataMember]
+		public string @base { get; set; }
+
+		[DataMember]
+		public Main main { get; set; }
+
+		[DataMember]
+		public int visibility { get; set; }
+
+		[DataMember]
+		public Wind wind { get; set; }
+
+		[DataMember]
+		public Clouds clouds { get; set; }
+
+		[DataMember]
+		public int dt { get; set; }
+
+		[DataMember]
+		public Sys sys { get; set; }
+
+		[DataMember]
+		public int id { get; set; }
+
+		[DataMember]
+		public string name { get; set; }
+
+		[DataMember]
+		public int cod { get; set; }
+	}
+
+	[DataContract]
+  public class Coord
+  {
+		[DataMember]
+		public int lon { get; set; }
+
+		[DataMember]
+		public double lat { get; set; }
+	}
+
+
+	[DataContract]
+	public class WeatherI
+	{
+		[DataMember]
+		public int id { get; set; }
+
+		[DataMember]
+		public string main { get; set; }
+
+		[DataMember]
+		public string description { get; set; }
+
+		[DataMember]
+		public string icon { get; set; }
+	}
+
+
+	[DataContract]
+	public class Main
+	{
+		[DataMember]
+		public double temp { get; set; }
+
+		[DataMember]
+		public int pressure { get; set; }
+
+		[DataMember]
+		public int humidity { get; set; }
+
+		[DataMember]
+		public double temp_min { get; set; }
+
+		[DataMember]
+		public double temp_max { get; set; }
+	}
+
+
+	[DataContract]
+	public class Wind
+	{
+		[DataMember]
+		public double speed { get; set; }
+
+		[DataMember]
+		public int deg { get; set; }
+	}
+
+
+	[DataContract]
+	public class Clouds
+	{
+		[DataMember]
+		public int all { get; set; }
+	}
+
+
+	[DataContract]
+	public class Sys
+	{
+		[DataMember]
+		public int type { get; set; }
+
+		[DataMember]
+		public int id { get; set; }
+
+		[DataMember]
+		public double message { get; set; }
+
+		[DataMember]
+		public string country { get; set; }
+
+		[DataMember]
+		public int sunrise { get; set; }
+
+		[DataMember]
+		public int sunset { get; set; }
+	}
+
+}
---
+++ KNMI-Common/OpenWeatherService.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeatherMonitor.Models;
+
+namespace WeatherMonitor.Data
+{
+  public class OpenWeatherService
+  {
+    public async Task<OpenWeather> GetOpenWeatherAsync()
+    {
+			//Get JSon string from the web
+			HttpClient http = new HttpClient();
+			HttpResponseMessage httpRespond =
+				await http.GetAsync($"http://api.openweathermap.org/data/2.5/weather?lat=51.913580&lon=4.999771&appid=8b6fbc4fab942058a2e1967b913460a4&lang=nl");
+			string httpResult = await httpRespond.Content.ReadAsStringAsync();
+
+			OpenWeather openWeather = JsonConvert.DeserializeObject<OpenWeather>(httpResult);
+
+			return openWeather;
+		}
+	}
+}
